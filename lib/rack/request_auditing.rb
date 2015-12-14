bcd30ff0cdf70c4f211c09208168175acfcd5028
Kyle Chong <kyle.chong@acorns.com>
2015-12-14 15:05:43 -0800

    Use explicit require instead of autoload
    Autoload is likely to be deprecated; see https://bugs.ruby-lang.org/issues/5653

--- lib/rack/request_auditing.rb
+++ lib/rack/request_auditing.rb
@@ -1,16 +1,12 @@
-require 'request_auditing/logger'
+require 'rack/request_auditing/auditor'
+require 'rack/request_auditing/header_processor'
+require 'rack/request_auditing/id'
+require 'rack/request_auditing/version'
 
 module Rack
   module RequestAuditing
-    autoload :Auditor,         'rack/request_auditing/auditor'
-    autoload :HeaderProcessor, 'rack/request_auditing/header_processor'
-    autoload :Id,              'rack/request_auditing/id'
-    autoload :Version,         'rack/request_auditing/version'
-
-    LOGGER_KEY = 'rack.logger'.freeze
-
-    def self.new(app, options = {})
-      Auditor.new(app, options)
+    def self.new(app)
+      Auditor.new(app)
     end
   end
 end

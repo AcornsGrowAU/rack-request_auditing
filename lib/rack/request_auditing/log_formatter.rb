2d3ada6c436cb15f3aff45455657f545f68a9274
Kyle Chong <kyle.chong@acorns.com>
2015-12-21 15:38:24 -0800

    Remove context annotation concerns from LogFormatter
    The sole concern of this log formatter should be formatting available data.
    The ContextLogger and external loggers own the responsibility of including
    the context tags in the message, so this formatter is now generic and can be
    shared regardless of context.

--- lib/rack/request_auditing/log_formatter.rb
+++ lib/rack/request_auditing/log_formatter.rb
@@ -4,25 +4,17 @@ module Rack
   module RequestAuditing
     class LogFormatter < ::Logger::Formatter
       DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S,%L'.freeze
+      MESSAGE_FORMAT = "%{time} [%{progname}] %{severity} %{msg}\n".freeze
 
       def initialize
         @datetime_format = DATETIME_FORMAT
       end
 
-      def call(severity, datetime, progname, msg)
+      def call(severity, time, progname, msg)
         msg_str = msg2str(msg)
-        tagged_message = Rack::RequestAuditing::MessageAnnotator.annotate(msg_str, context_tags)
-        timestamp = datetime.strftime(@datetime_format)
-        message = "#{timestamp} [#{progname}] #{severity} #{tagged_message}\n"
+        timestamp = time.strftime(@datetime_format)
+        message = MESSAGE_FORMAT % { time: timestamp, progname: progname, severity: severity, msg: msg_str }
         return message
-      end
-
-      def context_tags
-        tags = {}
-        tags[:correlation_id] = Rack::RequestAuditing::ContextSingleton.correlation_id
-        tags[:request_id] = Rack::RequestAuditing::ContextSingleton.request_id
-        tags[:parent_request_id] = Rack::RequestAuditing::ContextSingleton.parent_request_id
-        return tags
       end
     end
   end
--- spec/rack/request_auditing/log_formatter_spec.rb
+++ spec/rack/request_auditing/log_formatter_spec.rb
@@ -21,15 +21,6 @@ describe Rack::RequestAuditing::LogFormatter do
       subject.call(severity, datetime, progname, msg)
     end
 
-    it 'annotates the message with context tags' do
-      allow(subject).to receive(:msg2str).with(msg).and_return('FOOBAR')
-      context_tags = double('context tags')
-      allow(subject).to receive(:context_tags).and_return(context_tags)
-      expect(Rack::RequestAuditing::MessageAnnotator).to receive(:annotate)
-        .with('FOOBAR', context_tags)
-      subject.call(severity, datetime, progname, msg)
-    end
-
     it 'formats the datetime with the datetime format' do
       datetime_format = double('datetime format string')
       subject.instance_variable_set(:@datetime_format, datetime_format)
@@ -41,49 +32,9 @@ describe Rack::RequestAuditing::LogFormatter do
       allow(subject).to receive(:msg2str).with(msg).and_return('FOOBAR')
       context_tags = double('context tags')
       allow(subject).to receive(:context_tags).and_return(context_tags)
-      allow(Rack::RequestAuditing::MessageAnnotator).to receive(:annotate)
-        .with('FOOBAR', context_tags).and_return('FOOBAR {tag="value"}')
       allow(datetime).to receive(:strftime).and_return('FORMATTEDTIME')
       expect(subject.call(severity, datetime, progname, msg))
-        .to eq "FORMATTEDTIME [PROGNAME] SEVERITY FOOBAR {tag=\"value\"}\n"
-    end
-  end
-
-  describe '#context_tags' do
-    it 'gets correlation id from current context' do
-      expect(Rack::RequestAuditing::ContextSingleton)
-        .to receive(:correlation_id)
-      subject.context_tags
-    end
-
-    it 'gets request id from current context' do
-      expect(Rack::RequestAuditing::ContextSingleton).to receive(:request_id)
-      subject.context_tags
-    end
-
-    it 'gets parent request id from current context' do
-      expect(Rack::RequestAuditing::ContextSingleton)
-        .to receive(:parent_request_id)
-      subject.context_tags
-    end
-
-    it 'returns a hash of the current context attributes' do
-      correlation_id = double('correlation id')
-      request_id = double('request id')
-      parent_request_id = double('parent request id')
-      allow(Rack::RequestAuditing::ContextSingleton).to receive(:correlation_id)
-        .and_return(correlation_id)
-      allow(Rack::RequestAuditing::ContextSingleton).to receive(:request_id)
-        .and_return(request_id)
-      allow(Rack::RequestAuditing::ContextSingleton)
-        .to receive(:parent_request_id).and_return(parent_request_id)
-      expect(subject.context_tags).to eq(
-        {
-          correlation_id: correlation_id,
-          request_id: request_id,
-          parent_request_id: parent_request_id
-        }
-      )
+        .to eq "FORMATTEDTIME [PROGNAME] SEVERITY FOOBAR\n"
     end
   end
 end
